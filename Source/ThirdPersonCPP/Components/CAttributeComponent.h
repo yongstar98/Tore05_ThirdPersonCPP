74eda33d093e71e476b2acdf9f735bdd900c824a
Jay246h <babojeon641@gmail.com>
2024-06-20 17:50:26 +0900

    0618_Class

--- Source/ThirdPersonCPP/Components/CAttributeComponent.h
+++ Source/ThirdPersonCPP/Components/CAttributeComponent.h
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -13,16 +11,13 @@ class THIRDPERSONCPP_API UCAttributeComponent : public UActorComponent
 	GENERATED_BODY()
 
 public:	
-	// Sets default values for this component's properties
 	UCAttributeComponent();
 
 protected:
-	// Called when the game starts
 	virtual void BeginPlay() override;
 
 public:	
-	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+	/	
 };
