44f9411a405e7f96a39295093d77bd46fbb426c0
Jay246h <babojeon641@gmail.com>
2024-06-20 17:49:51 +0900

    0618_Class

--- Source/ThirdPersonCPP/ThirdPersonCPPGameModeBase.cpp
+++ Source/ThirdPersonCPP/ThirdPersonCPPGameModeBase.cpp
@@ -1,5 +1,9 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
 
 
 #include "ThirdPersonCPPGameModeBase.h"
+#include "Global.h"
 
+AThirdPersonCPPGameModeBase::AThirdPersonCPPGameModeBase()
+{
+	CHelpers::GetClass<APawn>(&DefaultPawnClass, "Blueprint'/Game/Player/BP_CPlayer.BP_CPlayer_C'");
+}
